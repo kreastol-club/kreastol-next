ac5eb563badb82394ebb293ca3f33cf70279cb22
Joshua Hegedus <josh.hegedus@outlook.com>
2023-10-01 09:43:37 +0200

    fixed styles

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# Here is the list of the remaining ideas
+
+- [ ] Add a working dynamic events page
+- [ ] Authorization
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,18 +2,20 @@ import Link from 'next/link';
 import React from 'react'
 
 const Nav = () => {
-  return (
-    <>
-      <div className="w-full flex flex-row items-center">
-        <h2 className='unique_header text-4xl flex'>Kreastol</h2>
-        <div className='w-full flex flex-1 justify-center items-center'>
-          <Link key={'home'} className="px-4 py-1 mx-1 btn" href={'/'}>Home</Link>
-          <Link key={'events'} className="px-4 py-1 mx-1 btn" href={'/events'}>Events</Link>
-        </div>
-        <span className="badge badge-primary badge-outline align-middle mr-2">preview</span>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="w-full sm:flex flex-row sm:items-center relative">
+                <div className="max-sm:relative">
+                    <h2 className='unique_header text-4xl flex m-2'>Kreastol</h2>
+                    <span className="badge badge-primary badge-outline absolute right-2 top-1/2 transform -translate-y-1/2">preview</span>
+                </div>
+                <div className='w-full flex flex-1 justify-center items-center'>
+                    <Link key={'home'} className="mx-1 btn" href={'/'}>Home</Link>
+                    <Link key={'events'} className="mx-1 btn" href={'/events'}>Events</Link>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Nav;
