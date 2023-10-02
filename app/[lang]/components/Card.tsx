a8fa0826c50e8c9a1b0b398aaf941a0a1a7fd46d
Joshua Hegedus <josh.hegedus@outlook.com>
2023-10-03 00:38:14 +0200

    added missing style

--- app/[lang]/components/Card.tsx
+++ app/[lang]/components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({children}: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-1 bg-base-300 rounded-md p-2 m-1">
+        <div className="flex flex-1 bg-base-300 rounded-md p-2 m-1 overflow-x-auto">
           {children}
         </div>
     )
