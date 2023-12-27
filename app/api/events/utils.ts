128b84fc4187d62e2208d669319e547d1f00c41d
Joshua Hegedus <josh.hegedus@outlook.com>
2023-12-27 12:40:38 +0100

    utils.ts error fix

--- app/api/events/utils.ts
+++ app/api/events/utils.ts
@@ -3,14 +3,9 @@ import { EventType } from "@/app/[lang]/events/page";
 export function getFilteredEvents(events: EventType[], locale: string, isAdmin: boolean) {
   const currentDate = new Date();
 
-  const getDayName = (date: Date, locale: string) => {
-    return date.toLocaleDateString(locale, { weekday: 'long' });
-  };
-
   events = events.filter((event) => {
-    if (event.date) {
-      const eventDate = new Date(event.date);
-      event.dayName = getDayName(eventDate, locale);
+    if (event.end) {
+      const eventDate = new Date(event.end);
       return eventDate >= currentDate;
     }
     return true;
